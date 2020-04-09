445a5188a5283081677a9b3ff7f348284cf76891
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-04-09 13:35:07 -0500

    refactor for easier isolation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import { 
-  ApolloClient, 
-  ApolloProvider,
-  HttpLink, 
-  InMemoryCache,
-} from '@apollo/client'
 import ExchangeRates from './components/ExchangeRates'
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'https://48p1r2roz4.sse.codesandbox.io'
-  })
-})
+import AppProvider from './apollo/apolloClient'
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <AppProvider>
       <div>
         <h2>My first Apollo app 🚀</h2>
         <ExchangeRates />
       </div>
-  </ApolloProvider>
+  </AppProvider>
   )
 }
 
