7542504b5165476a4649b6b1d962948bdb8b33ac
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-04-09 13:37:05 -0500

    example using data

---
+++ src/components/ExchangeRates.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ExchangeRates from './ExchangeRates'
+import AppProvider from '../apollo/apolloClient'
+
+export default { title: 'ExchangeRates'}
+
+export const withTest = () => <AppProvider><ExchangeRates /></AppProvider>
